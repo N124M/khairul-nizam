fb3da802efea03d6929dfa2bbc584efd64c59481
N124M <85291171+N124M@users.noreply.github.com>
2021-06-04 01:31:13 +0700

    Update and rename README.md to persegi.java

---
+++ persegi.java
@@ -0,0 +1,39 @@
+# khairul-nizam
+public class persegi {
+   int panjang;
+   int lebar;
+    
+   public int getLebar(){
+       return lebar;
+   }
+   public void setLebar(int lebar){
+       this.lebar = lebar;
+   }
+   public int getPanjang(){
+       return panjang;
+   }
+   public void setPanjang(int panjang){
+       this.panjang = panjang;
+   }
+   public persegi(){
+       int p = 0;
+       int l = 0;
+   }
+   
+   public int luas(int p, int l){
+       return p * l;
+   }
+   public int getLuas(){
+       return luas(panjang, lebar);
+   }
+  public class hasilpersegi {
+  public static void main(String[] args){
+      persegi p = new persegi();
+      p.setPanjang(10);
+      p.setLebar(60);
+      System.out.println("panjang : "+p.getPanjang());
+      System.out.println("lebar : "+p.getLebar());
+      System.out.println("luas persegi : "+p.getLuas());
+  }
+}
+}
